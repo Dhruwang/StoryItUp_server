4751b2dfd1fddb78f5a663feafd2df0684453298
Dhruwang <dhruwangjariwala18@gmail.com>
2023-02-11 15:04:37 +0530

    paraphrase and aisearch

--- Routes/investor.route.js
+++ Routes/investor.route.js
@@ -1,10 +1,11 @@
 const express = require("express")
-const {saveInvestor,fetchInvestors} = require("../controllers/investor.controller")
+const {saveInvestor,fetchInvestors,paraphrase} = require("../controllers/investor.controller")
 const investorRouter = express.Router()
 const { body, validationResult } = require('express-validator');
 
 investorRouter.post("/saveInvestor",saveInvestor)
 investorRouter.get("/fetchInvestors",fetchInvestors)
+investorRouter.post("/paraphrase",paraphrase)
 
 module.exports = {
     investorRouter
--- controllers/investor.controller.js
+++ controllers/investor.controller.js
@@ -1,5 +1,12 @@
 const express = require('express')
 const Investor = require('../models/Investor')
+const { Configuration, OpenAIApi } = require("openai");
+
+const configuration = new Configuration({
+    apiKey: "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"
+});
+
+
 
 async function saveInvestor(req,res){ 
     try {
@@ -31,8 +38,30 @@ async function fetchInvestors(req,res) {
     }
     
 }
+ async function paraphrase (req, res) {
+    try {
+        const openai = new OpenAIApi(configuration)
+
+        const completion = openai.createCompletion({
+            model: 'text-davinci-003',
+            prompt: req.body.search,
+            max_tokens: 100
+        })
+
+        completion.then((r) => {
+            res.send(r.data.choices[0].text)
+        })
+
+
+    } catch (error) {
+        console.log(error.message)
+        res.status(500).send("internal server error")
+    }
+
+}
+
 
 
 module.exports ={
-    saveInvestor,fetchInvestors
+    saveInvestor,fetchInvestors,paraphrase
 }
